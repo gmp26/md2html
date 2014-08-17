92ba3566a7bd346f4b1466220ebc7a2176a316e4
gmp26 <gmp26@cam.ac.uk>
2014-08-17 13:44:15 +0100

    add markdown file globber

--- md2html.hs
+++ md2html.hs
@@ -1,5 +1,29 @@
 module Main where
 
+import System.Directory (doesFileExist, getDirectoryContents)
+import System.FilePath.Glob (globDir1, compile)
+import System.FilePath ((</>))
+
+-- Apply glob pattern to yield a list of markdown files in a directory
+markdownFiles :: FilePath -> IO [FilePath]
+markdownFiles dir = globDir1 (compile "**/*.md") dir
+
+process :: FilePath -> IO ()
+process = putStrLn
+
+-- | Walk a directory, selecting files, and processing
+mapDir :: (FilePath -> IO ()) -> FilePath -> IO ()
+mapDir proc fp = do
+  isFile <- doesFileExist fp -- is a file of fp
+  if isFile then proc fp     -- process the file
+  else getDirectoryContents fp >>=
+       mapM_ (mapDir proc . (fp </>)) . filter (`notElem` [".", ".."])
+
+-- main = mapDir process "."
+
+
 main :: IO()
-main =
-  putStrLn "MarkDown to HTML"
+main = do
+  putStrLn "Hi"
+  files <- markdownFiles ".."
+  putStrLn $ show files
